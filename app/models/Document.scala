12ee2bde8253c55465a639e2769d0b00c4947f0e
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-11-15 18:53:31 +0100

    Added tags field

--- app/models/Document.scala
+++ app/models/Document.scala
@@ -3,14 +3,29 @@ package models
 import play.api.data._
 import play.api.data.Forms._
 
-case class Document(title: String, department: Option[String], link: String)
+/**
+ * A document is the basic class which is inserted inside
+ * the database. It consists of all basic features to search
+ * for documents
+ * <ul>
+ *   <li>Name</li>
+ *   <li>Department</li>
+ *   <li>Link</li>
+ *   <li>Tags</li>
+ * </ul>
+ */
+case class Document(title: String, department: Option[String], link: String, tags: String) {
+  
+  def tagsAsList = tags.split(",").toList.map(_.trim)
+}
 
 object Document {
 
   val form = Form(
     mapping("title" -> nonEmptyText,
       "department" -> optional(text),
-      "link" -> text)
+      "link" -> text,
+      "tags" -> text)
       
       (Document.apply)(Document.unapply))
       
